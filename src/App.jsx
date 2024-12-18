e965a3638d5b6d6637cb13c7974caf12f88b730f
eduardoamorim-dev <amorimm.dev@gmail.com>
2024-12-17 23:41:08 -0300

    feat: add splash screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
 // App.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./Home/Home";
 import NotFound from "./components/NotFound/NotFound";
+import SplashScreen from "./components/SplashScreen/SplashScreen";
 
 function App() {
-    return (
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSplash(false), 2000); 
+    return () => clearTimeout(timer); 
+  }, []);
+
+  return (
+    <>
+      {showSplash ? (
+        <SplashScreen />
+      ) : (
         <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/notfound" element={<NotFound />} />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/notfound" element={<NotFound />} />
+          </Routes>
         </Router>
-    );
+      )}
+    </>
+  );
 }
 
 export default App;
